4f3a6f9b4aedb2723cc0462729bcc7c3e5bbfaea
Ian-Stentz <ianstentz25@gmail.com>
2024-08-29 22:19:43 -0700

    Encapsulating grid logic

--- grid.js
+++ grid.js
@@ -1,1 +1,24 @@
 //Keep the grid here and make it an object, pronto 
+class Grid {
+    constructor(xOffset, yOffset, cellSize, rows, columns) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        this.cellSize = cellSize;
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    placePoints(ptAmnt, minRadius, maxRadius) {
+        this.ptAmnt = ptAmnt;
+        this.minRadius = minRadius;
+        this.maxRadius = maxRadius;
+    }
+
+    selectColors() {
+
+    }
+
+    makeRectangles() {
+
+    }
+}
